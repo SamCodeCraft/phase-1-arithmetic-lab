ad6dd8ef9e867b40195888693907f0e04152864a
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-03-20 19:59:05 +0300

    Created Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+//creation of variable called multiply that will multiply varaibsles num1 and num2 to give result =62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+//creation of a variable called random that generates integer greater than 0.
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+// creating a variable called mod that will calculate the remainder of dividing num3 by num4 to give a remainder dof 4.
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+// creating a variable called max that finds the highest numer in a set and the value returned should be 20.
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
